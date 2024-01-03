8b5dfb2413dd74391bfbffc8ab49346afbc3b286
sdhabal100 <sdhabal100@gmail.com>
2024-01-04 03:37:55 +0530

    127:create save post feature

---
+++ src/main/java/com/myblog5/myblog5/Myblog5Application.java
@@ -0,0 +1,13 @@
+package com.myblog5.myblog5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog5/myblog5/Myblog5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog5.myblog5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
